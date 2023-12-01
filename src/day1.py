c8965f8f5646a0a86589541f4eb3de3731b8a895
Aaryan <aaryan.mshra@gmail.com>
2023-12-01 18:53:52 +0530

    solved day1

--- src/day1.py
+++ src/day1.py
@@ -1,19 +1,18 @@
 def calibration_value(word):
     hmap = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
-    num = ''
+    num = []
 
     for i in range(len(word)):
+        if word[i].isdigit():
+            num.append(word[i])
         if word[i:i+3] in hmap:
-            num += str(hmap[word[i:i+3]])
-            break
+            num.append(hmap[word[i:i+3]])
         if word[i:i+4] in hmap:
-            num += str(hmap[word[i:i+3]])
-            break
+            num.append(hmap[word[i:i+4]])
         if word[i:i+5] in hmap:
-            num += str(hmap[word[i:i+3]])
-            break
+            num.append(hmap[word[i:i+5]])
 
-    return 
+    return int(str(num[0]) + str(num[len(num)-1]))
 
 with open('data/day1.txt', 'r') as file:
     arr = file.readlines()
@@ -23,4 +22,4 @@ with open('data/day1.txt', 'r') as file:
     for word in arr:
         values.append(calibration_value(word[:len(word) - 1]))
 
-    print(values)
+    print(sum(values))
