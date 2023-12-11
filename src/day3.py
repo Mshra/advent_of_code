4d3ba445f87492ce0f8e26e7ddae05a04b472aee
Aaryan <aaryan.mshra@gmail.com>
2023-12-12 01:38:45 +0530

    day3.py

--- src/day3.py
+++ src/day3.py
@@ -1,6 +1,36 @@
+def show_engine(schematic):
+    for i in range(0, 100, 10):
+        print(schematic[i:i+10])
+
+def find_number(schematic):
+    def parse_number(string):
+        num = ''
+        for c in string:
+            if c.isdigit(): num += c
+        return int(num)
+
+    not_symbol = set([n for n in range(0, 10)] + ['.'])
+    numbers = []
+
+    for i in range(len(schematic)):
+        if schematic[i] not in not_symbol:
+            # check left adjacent
+            if schematic[i - 1].isdigit():
+                numbers.append(parse_number(schematic[i-3 : i]))
+
+            # check right adjacent
+            if schematic[i + 1].isdigit():
+                numbers.append(parse_number(schematic[i+1 : i+4]))
+
+            # check diagonally(previous)
+            
+
+            # check diagonally(after)
+
 if __name__ == '__main__':
-    with open('data/day3.txt') as file:
+    with open('data/day3.txt', 'r') as file:
         f = file.readlines()
 
-        for engine_schematic in f:
-            engine_schematic = engine_schematic[:len(engine_schematic) - 1]
+        for schematic in f:
+            show_engine(schematic)
+            break
