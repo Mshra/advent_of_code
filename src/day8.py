243ae1e4b0b78a461ee8b82057ef0b47948fae33
Aaryan <aaryan.mshra@gmail.com>
2023-12-20 21:12:32 +0530

    yeah

--- src/day8.py
+++ src/day8.py
@@ -10,3 +10,23 @@ with open('data/day8.txt', 'r') as file:
     instruction = file.readline()[:281]
     file.readline() # to jump over blank line
     desert_map = list(map(func, file.readlines()))
+
+    d_map = {
+        'L': 0,
+        'R': 1
+    }
+    steps = 0
+
+    location = desert_map[find_location('AAA', desert_map)]
+
+    for direction in instruction:
+        print(location)
+        if location[:3] == 'ZZZ':
+            print(steps)
+
+        if d_map[direction]: # take right
+            location = desert_map[find_location(location[12:15], desert_map)]
+        else: # take left
+            location = desert_map[find_location(location[7:10], desert_map)]
+
+        steps += 1
